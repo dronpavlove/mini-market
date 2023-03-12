78b6235f7467fa5709205c4a48f7229de5e89178
dronpavlove <pavlove75@mail.ru>
2023-03-12 20:25:17 +0300

    корректировка отображения товара в API

--- minimarket/site_api/serializers.py
+++ minimarket/site_api/serializers.py
@@ -15,34 +15,28 @@ class CategorySerializers(serializers.ModelSerializer):
         fields = ['id', 'category_name', 'icon_photo']
 
 
-class PropertyProductSerialaizers(serializers.ModelSerializer):
+class PropertySerializers(serializers.ModelSerializer):
     class Meta:
-        model = PropertyProduct
-        fields = "__all__"
-
-
-class PropertySerialaizers(serializers.ModelSerializer):
-    class Meta:
-        value = PropertyProductSerialaizers(many=True, read_only=True)
         model = Property
         fields = "__all__"
+
+
+class PropertyProductSerializers(serializers.ModelSerializer):
+    property = serializers.StringRelatedField()
+
+    class Meta:
+        # value = PropertySerializers(many=True, read_only=True)
+        model = PropertyProduct
+        fields = ['property', 'value']
 
 
 class ProductProfileSerializers(serializers.ModelSerializer):
     product_photo = serializers.StringRelatedField(many=True)
     # properties = serializers.StringRelatedField(many=True)
-    properties = PropertySerialaizers(many=True, read_only=True)
+    product_properties = PropertyProductSerializers(many=True, read_only=True)
     category = serializers.StringRelatedField()
-    # category = serializers.SerializerMethodField('product')
-    #
-    # def product_in_shop(self, request):
-    #     queryset = []
-    #     for i in ProductInShop.objects.filter(name_id=request.id):
-    #         queryset.append({'shop_id': i.in_shop.id, 'shop_name': i.in_shop.name, 'amount': i.amount})
-    #     return queryset
 
     class Meta:
         model = Product
-        # queryset = Product.objects.all().select_related('category').prefetch_related("product_photo")
-        fields = ['id', 'article', 'category', 'name', 'properties',
-                  'description', "product_photo", 'price', 'amount']  # , 'in_shop']'photo',
+        fields = ['id', 'article', 'category', 'name',  # 'properties',
+                  'description', "product_photo", 'price', 'amount', 'product_properties']  # , 'in_shop']'photo',
